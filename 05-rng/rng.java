af7f133baffa703313398117bbd4d3faa881afd2
Harryw14 <114939385+Harryw14@users.noreply.github.com>
2022-10-12 11:55:23 -0400

    Add files via upload

---
+++ 05-rng/rng.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class rng here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+import java.util.Random;
+public class rng
+{
+  public static void main(String[]args){
+    Scanner s= new Scanner(System.in);
+    Random r=new Random();
+    System.out.println("What gamemode do you want\neasy\nmedium\nhard");
+    String mode= s.nextLine();
+    if (mode.equals("easy")){
+     int min=0;
+     int max=10;
+  } else if(mode.equals("medium")){
+     int min=0;
+     int max=50;
+    }
+     else if(mode.equals("hard")){
+     int min=0;
+  int max=100;
+}
+    System.out.println("Enter your guess");
+    int pg=s.nextInt();//pg=player guess
+    if(pg!=mn){
+     if(pg<mn){
+      System.out.println("you were off by"+(mn-pg));
+    }
+     if(pg>mn){
+      System.out.println("you were off by"+(pg-mn)); 
+     }
+    else{
+       System.out.println("Correct");
+    } 
+  }
+}
